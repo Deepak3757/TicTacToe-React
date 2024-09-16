f2c4fe2b87defc0bb3e25e791645c3ca6a9ddd6f
Deepak gupta <deepakgupta70363@gmail.com>
2024-09-16 09:03:00 +0530

    Removed active player state and deriving the active player with the help of turns

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+
+
+function deriveActivePlayer(gameTurns){
+  let currPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player == "X") currPlayer = "O";
+  return currPlayer;
+}
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
-  function handleSelectSquare(rowInd, colInd) {
-    setActivePlayer((currPlayer) => (currPlayer === "X" ? "O" : "X"));
-    setGameTurns((prevTurns) => {
-      let currPlayer = "X";
 
-      if (prevTurns.length > 0 && prevTurns[0].player == "X") currPlayer = "O";
+  const activePlayer=deriveActivePlayer(gameTurns);
+
+
+  function handleSelectSquare(rowInd, colInd) {
+
+    setGameTurns((prevTurns) => {
+      const currPlayer=deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowInd, col: colInd }, player: currPlayer },
